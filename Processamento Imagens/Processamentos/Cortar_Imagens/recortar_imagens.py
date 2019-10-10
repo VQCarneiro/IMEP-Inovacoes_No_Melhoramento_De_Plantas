e82f0668620bf58dcd90ed13773bba66bf6ecbc8
Vinícius Quintão Carneiro <vqcarneiro@gmail.com>
2019-10-10 07:45:48 -0300

    Atualização Processamento
    data: 10/10/2019
    Desenvolvedor: Vinícius Q. Carneiro
    Procediemnto: Recortar Imagens

---
+++ Processamento Imagens/Processamentos/Cortar_Imagens/recortar_imagens.py
@@ -0,0 +1,54 @@
+#############################################################################################
+# Cabeçalho
+# Data: 10/03/2019
+# Horário: 10:51
+# Desenvolvedor: Vinícius Quintão Carneiro
+# Imagens: Abóboras
+# Propietário das imagens: Ronaldo Machado Júnior
+# Procedimento: Recortar imagens
+#############################################################################################
+# Importar pacotes
+import cv2 # Importa o pacote opencv
+import numpy as np # Importa o pacote numpy
+from matplotlib import pyplot as plt # Importa o pacote matplotlib
+#import imutils
+########################################################################################################################
+# Leitura da imagem
+img = cv2.imread('exemplo_01.jpg',1) # Carrega imagem colorida em BGR
+img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+lin, col, canais = np.shape(img)
+print('-----------------------------------------------------------------------------------------------------------------')
+print('Dimensão: ' + str(lin) +' x '+ str(col))
+print(' ')
+print('Número de Canais: ' + str(canais))
+########################################################################################################################
+# Cropping - Recortar partes de imagens
+img_recorte = img[3170:3360,535:828]
+print(img[0,0,:])
+print(' ')
+print('-----------------------------------------------------------------------------------------------------------------')
+print('Matriz de Pixels')
+print(img[3170:3180,535:545,1])
+print(' ')
+print('-----------------------------------------------------------------------------------------------------------------')
+print('Matriz de Pixels')
+print(img[3180:3200,3190:3200,0])
+print('-----------------------------------------------------------------------------------------------------------------')
+########################################################################################################################
+# Apresentar imagens na tela
+
+plt.figure('Transformações imagens')
+plt.subplot(1,2,1)
+plt.imshow(img)
+plt.title('Imagem')
+
+plt.subplot(1,2,2)
+plt.imshow(img_recorte)
+plt.title('Recorte')
+
+plt.show()
+########################################################################################################################
+
+
+
+
